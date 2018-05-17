7a8b0cd85994fc32f0825edcd68f1e168f5ecf7e
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 10:58:56 +0100

    Added classes for dropdown to kick in, css back to normal check trigger

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -3,10 +3,10 @@ $(function() {
 
    $('#prod-selector').attr("href","#")
    $('#prod-selector, #prod-cta').click(function() {
-   $('#prod-selector').parent().toggleClass('chameleon no-anim');
-   $(".product-selector-dropdown" ).slideToggle('easeInOutQuint')
+   $('#prod-selector').parent().toggleClass('');
+   $(".menu-selector" ).slideToggle('easeInOutQuint')
    $("#block-block-11").slideToggle('easeInOutQuint')
-     $('#prod-selector').parent().removeClass('no-anim')
+     $('#prod-selector').parent().removeClass('')
 
     })
 
