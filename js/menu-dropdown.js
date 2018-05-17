2da35a8c1450d479f6eccc6a996773256296c3b2
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 11:34:42 +0100

    Adding the js and html for product to have a similar dropdown menu

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -4,10 +4,24 @@ $(function() {
    $('#prod-selector').attr("href","#")
    $('#prod-selector, #prod-cta').click(function() {
    $('#prod-selector').parent().toggleClass('');
-   $(".menu-selector" ).slideToggle('easeInOutQuint')
+   $(".prod-selector" ).slideToggle('easeInOutQuint')
    $("#block-block-11").slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
 
     })
 
   });(jQuery)
+
+  $(function() {
+
+
+     $('#ind-selector').attr("href","#")
+     $('#ind-selector, #prod-cta').click(function() {
+     $('#ind-selector').parent().toggleClass('');
+     $(".menu-selector" ).slideToggle('easeInOutQuint')
+     $("#block-block-11").slideToggle('easeInOutQuint')
+       $('#ind-selector').parent().removeClass('')
+
+      })
+
+    });(jQuery)
