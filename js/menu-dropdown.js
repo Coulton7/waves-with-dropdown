f04369a83c3e44a454b3380d34df4b8ec2586510
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 12:45:20 +0100

    reset for other idea

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -6,7 +6,6 @@ $(function() {
    $('#prod-selector').parent().toggleClass('');
    $(".prod-selector" ).slideToggle('easeInOutQuint')
    $("#block-block-11").slideToggle('easeInOutQuint')
-   $(".ind-selector").slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
 
     })
@@ -16,7 +15,6 @@ $(function() {
     $('#ind-selector').parent().toggleClass('');
     $(".ind-selector" ).slideToggle('easeInOutQuint')
     $("#block-block-11").slideToggle('easeInOutQuint')
-    $(".prod-selector").slideToggle('easeInOutQuint')
       $('#ind-selector').parent().removeClass('')
     })
 
