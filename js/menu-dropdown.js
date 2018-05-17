293ec5d6b642502c11371afc6434107d486a0bde
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 16:38:15 +0100

    needed to change the dropdown identifier from class to id

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -7,8 +7,8 @@ $(function() {
    $('#prod-selector').parent().toggleClass('');
    $(".prod-selector" ).slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
-     $('.dropdown').toggleClass('active')
-     $('.dropdown').removeClass('active')
+     $('#dropdown').toggleClass('active')
+     $('#dropdown').removeClass('active')
    }
 
      else {
