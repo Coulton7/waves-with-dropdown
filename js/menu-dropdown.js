f874fd629791812e8d5dea17f68fa6a9b49ae31b
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 12:14:15 +0100

    adding the remove class for the other ahref selections

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -7,6 +7,7 @@ $(function() {
    $(".prod-selector" ).slideToggle('easeInOutQuint')
    $("#block-block-11").slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
+     $('#ind-selector').parent().removeClass('')
 
     })
 
@@ -21,6 +22,7 @@ $(function() {
      $(".ind-selector" ).slideToggle('easeInOutQuint')
      $("#block-block-11").slideToggle('easeInOutQuint')
        $('#ind-selector').parent().removeClass('')
+       $('#prod-selector').parent().removeClass('')
 
       })
 
