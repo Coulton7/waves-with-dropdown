9bb652025e581663613af46f601adfe77242c768
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-17 12:22:08 +0100

    reset

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -5,7 +5,7 @@ $(function() {
    $('#prod-selector').click(function() {
    $('#prod-selector').parent().toggleClass('');
    $(".prod-selector" ).slideToggle('easeInOutQuint')
-   $(".ind-selector").slideToggle('easeInOutQuint')
+   $("#block-block-11").slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
 
     })
@@ -19,7 +19,7 @@ $(function() {
      $('#ind-selector').click(function() {
      $('#ind-selector').parent().toggleClass('');
      $(".ind-selector" ).slideToggle('easeInOutQuint')
-     $(".prod-selector").slideToggle('easeInOutQuint')
+     $("#block-block-11").slideToggle('easeInOutQuint')
        $('#ind-selector').parent().removeClass('')
 
       })
