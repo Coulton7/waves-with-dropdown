efd02cf40d7c71931a13885b5e3c38290fd5a5f9
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-05-18 10:47:52 +0100

    Attempt at smarter js

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -7,7 +7,7 @@ $(function() {
    $('#prod-selector').parent().toggleClass('');
    $(".prod-selector" ).slideToggle('easeInOutQuint')
      $('#prod-selector').parent().removeClass('')
-     $('#dropdown').toggleClass('dropdown-fill')
+     $('#dropdown').removeClass('hidden-menu').addClass('visible-menu')
    }
 
      else {
