38a2ca71dd504d7e51de1a87bff930b03450a909
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-12 16:16:34 +0100

    Making ches to the js on the page

--- assets/js/front-overlay-trigger.js
+++ assets/js/front-overlay-trigger.js
@@ -1,22 +1,24 @@
+var x = document.getElementById("overlayNav");
+var y = document.getElementById("searchOverlay");
+var s = document.getElementById("shareOverlay");
+var overlayNavBtn = document.querySelector("#overlay-btn");
+var overlaySearchBtn = document.querySelector("#overlay-search");
+var overlaySearchGlobal = document.querySelector("#global-overlay-search");
+var overlayShareBtn = document.querySelector("#overlay-share");
+var footerOverlayBtn = document.querySelector("#footer-overlay-btn");
+var mobileNavOverlay = document.querySelector("[data-bs-target='#mobile-overlay']");
+var mobileOverlaySearch  = document.querySelector("#mob-overlay-search");
+var html = document.querySelector("html");
+var body = document.querySelector("body");
+var dialogOffCanvas = document.querySelector(".dialog-off-canvas-main-canvas");
+var frontPage = document.querySelector(".path-frontpage");
+var scrollNav = document.querySelector('.scroll-nav');
+var navLinks = document.querySelectorAll(".nav-link");
+var url = window.location.href;
+var query = 'q';
+var popSearch = document.querySelector("#popular-search-bar input");
+
 document.addEventListener("DOMContentLoaded", function () {
-  var x = document.getElementById("overlayNav");
-  var y = document.getElementById("searchOverlay");
-  var s = document.getElementById("shareOverlay");
-  var overlayNavBtn = document.querySelector("#overlay-btn");
-  var overlaySearchBtn = document.querySelector("#overlay-search");
-  var overlaySearchGlobal = document.querySelector("#global-overlay-search");
-  var overlayShareBtn = document.querySelector("#overlay-share");
-  var footerOverlayBtn = document.querySelector("#footer-overlay-btn");
-  var mobileNavOverlay = document.querySelector("[data-bs-target='#mobile-overlay']");
-  var mobileOverlaySearch  = document.querySelector("#mob-overlay-search");
-  var html = document.querySelector("html");
-  var body = document.querySelector("body");
-  var dialogOffCanvas = document.querySelector(".dialog-off-canvas-main-canvas");
-  var frontPage = document.querySelector(".path-frontpage");
-  var scrollNav = document.querySelector('.scroll-nav');
-  var navLinks = document.querySelectorAll(".nav-link");
-  var url = window.location.href;
-  var query = 'q';
   
   if(frontPage){
     if(overlayNavBtn){
@@ -364,6 +366,59 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector('.ais-SearchBox-input').focus();
           }
         });
+    }
+  }
+});
+
+popSearch.addEventListener("keypress", function(event) {
+  if(event.key === "Enter") {
+    event.preventDefault();
+    document.querySelector('#popular-search-bar .ais-SearchBox-submit').click()
+    overlaySearchBtn.classList.toggle("change");
+    if (y.style.height === "100%") {
+      y.style.height = "1px";
+      y.style.top = "-1px";
+      html.classList.remove("noscroll");
+      body.classList.remove("noscroll");
+      dialogOffCanvas.classList.remove("noscroll");
+      overlaySearchBtn.classList.add("search-btn");
+      if(window.innerWidth > 992) {
+        if (scrollNav.classList.contains("scrolled-up")){
+          scrollNav.classList.remove("scrolled-up");
+          scrollNav.classList.add("scrolled-down")
+        } else {
+          scrollNav.classList.add("scrolled-down")
+        }
+      }
+      document.querySelector('.overlay-btn').classList.remove("normText");
+    } else {
+      y.style.height = "100%";
+      y.style.top = "0";
+      if (x.style.height == "100%") {
+        x.style.height = "1px";
+        x.style.bottom = "-1px";
+        overlaySearchBtn.classList.remove("change");
+        overlaySearchBtn.classList.add("overlay-btn");
+      } else if (s.style.height == "100%") {
+        s.style.height = "1px";
+        s.style.bottom = "-1px";
+        overlayShareBtn.classList.remove("change");
+        overlayShareBtn.classList.add("share-btn");
+      }
+      html.classList.add("noscroll");
+      body.classList.add("noscroll");
+      dialogOffCanvas.classList.add("noscroll");
+      overlaySearchBtn.classList.remove("search-btn");
+      if(window.innerWidth > 992) {
+        if (scrollNav.classList.contains("scrolled-down")){
+          scrollNav.classList.remove("scrolled-down");
+          scrollNav.classList.add("scrolled-up")
+        } else {
+          scrollNav.classList.add("scrolled-up")
+        }
+      }
+      document.querySelector('.overlay-btn').classList.add("normText");
+      document.querySelector('.ais-SearchBox-input').focus();
     }
   }
 });
