37e3e36003c62d200efe145e185389dee1972d5a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-12 16:38:28 +0100

    Trying a more boken up targeting method

--- assets/js/front-overlay-trigger.js
+++ assets/js/front-overlay-trigger.js
@@ -16,7 +16,7 @@ var scrollNav = document.querySelector('.scroll-nav');
 var navLinks = document.querySelectorAll(".nav-link");
 var url = window.location.href;
 var query = 'q';
-var popSearch = document.querySelector("#popular-search-bar input");
+var popSearch = document.getElementById("popular-search-bar").querySelector("input");
 
 document.addEventListener("DOMContentLoaded", function () {
   
