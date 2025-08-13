b91c263d318874c44e2fa047b6beadb90319a3a0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-13 16:44:02 +0100

    Adjusting for taregting again

--- assets/js/front-overlay-trigger.js
+++ assets/js/front-overlay-trigger.js
@@ -16,7 +16,7 @@ var scrollNav = document.querySelector('.scroll-nav');
 var navLinks = document.querySelectorAll(".nav-link");
 var url = window.location.href;
 var query = 'q';
-var popSearch = document.querySelector("#popular-search-bar .pop-search-bar");
+var popSearch = document.querySelector("#popular-search-bar");
 var popSearchButton = document.querySelector("#popular-search-bar .pop-search-button");
 
 document.addEventListener("DOMContentLoaded", function () {
