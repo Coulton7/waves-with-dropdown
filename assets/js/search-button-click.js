8fc66b54bf6b9d754e2b1e82191b8ce366ead45b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-27 12:22:18 +0100

    back to basic test

--- assets/js/search-button-click.js
+++ assets/js/search-button-click.js
@@ -19,54 +19,8 @@ var query = 'q';
 
 document.addEventListener("DOMContentLoaded", function () {
   var popSearchButton = document.querySelector(".pop-search-button");
-
-      console.log('Pop search button is here')
-        popSearchButton.addEventListener("click", function popSearchBtn(){
-            overlaySearchBtn.classList.toggle("change");
-            if (y.style.height === "100%") {
-              y.style.height = "1px";
-              y.style.top = "-1px";
-          html.classList.remove("noscroll");
-          body.classList.remove("noscroll");
-          dialogOffCanvas.classList.remove("noscroll");
-          overlaySearchBtn.classList.add("search-btn");
-          if(window.innerWidth > 992) {
-            if (scrollNav.classList.contains("scrolled-up")){
-              scrollNav.classList.remove("scrolled-up");
-              scrollNav.classList.add("scrolled-down")
-            } else {
-              scrollNav.classList.add("scrolled-down")
-            }
-          }
-          document.querySelector('.overlay-btn').classList.remove("normText");
-        } else {
-          y.style.height = "100%";
-          y.style.top = "0";
-          if (x.style.height == "100%") {
-            x.style.height = "1px";
-            x.style.bottom = "-1px";
-            overlaySearchBtn.classList.remove("change");
-            overlaySearchBtn.classList.add("overlay-btn");
-          } else if (s.style.height == "100%") {
-            s.style.height = "1px";
-            s.style.bottom = "-1px";
-            overlayShareBtn.classList.remove("change");
-            overlayShareBtn.classList.add("share-btn");
-          }
-          html.classList.add("noscroll");
-          body.classList.add("noscroll");
-          dialogOffCanvas.classList.add("noscroll");
-          overlaySearchBtn.classList.remove("search-btn");
-          if(window.innerWidth > 992) {
-            if (scrollNav.classList.contains("scrolled-down")){
-              scrollNav.classList.remove("scrolled-down");
-              scrollNav.classList.add("scrolled-up")
-            } else {
-              scrollNav.classList.add("scrolled-up")
-            }
-          }
-          document.querySelector('.overlay-btn').classList.add("normText");
-          document.querySelector('.main-search-bar').focus();
-        }
-      });
+  if(popSearchButton){
+      console.log('Pop search button is here');
+  }
+        
 });
