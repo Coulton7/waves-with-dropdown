1436cde6c6d0a34ec7f0400eecce89797001584f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-08-27 11:38:24 +0100

    Trying as a const instead od a var

--- assets/js/front-overlay-trigger.js
+++ assets/js/front-overlay-trigger.js
@@ -17,7 +17,7 @@ var navLinks = document.querySelectorAll(".nav-link");
 var url = window.location.href;
 var query = 'q';
 var popSearch = document.querySelector("#popular-search-bar");
-var popSearchButton = document.querySelector(".pop-search-button");
+const popSearchButton = document.querySelector(".pop-search-button");
 
 document.addEventListener("DOMContentLoaded", function () {
   
