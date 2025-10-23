5790401c79db1d333eaabd997ddb11b01042bfcf
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-10-23 13:13:59 +0100

    Re-added routing as part of a test

--- assets/js/site-search.js
+++ assets/js/site-search.js
@@ -1696,6 +1696,72 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
                 }
             },
+            routing: {
+                router: instantsearch.routers.history({
+
+                    createURL({ qsModule, routeState, location }) {
+                        const { origin, pathname, hash} = location;
+                        const queryParameters = {};
+
+                        if(routeState.q) {
+                            queryParameters.q = encodeURIComponent(routeState.q);
+                        }
+                        if(routeState.type) {
+                            queryParameters.type = routeState.type.map(encodeURIComponent);
+                        }
+                        if(routeState.lang) {
+                            queryParameters.lang = routeState.lang.map(encodeURIComponent);
+                        }
+
+                        const queryString = qsModule.stringify(queryParameters, {
+                            addQueryPrefix: true,
+                            arrayFormat: 'indices'
+                        });
+
+                        return `${origin}${pathname}${queryString}`;
+                    },
+
+                    parseUrl({ qsModule, location }) {
+                        const { q = '', type = [], lang =[] } = qsModule.parse(
+                            location.search.slice(1)
+                        );
+                        const allType = Array.isArray(type)
+                            ? type
+                            : [type].filter(Boolean);
+                        const allLang = Array.isArray(lang)
+                            ? lang
+                            :[lang].filter(Boolean);
+                        return {
+                            q: decodeURIComponent(q),
+                            type: allType.map(decodeURIComponent),
+                            lang: allLang.map(decodeURIComponent)
+                        };
+                    },
+                    writeDelay: 400,
+                    cleanUrlOnDispode: true,
+                }),
+                stateMapping: {
+                    stateToRoute(uiState){
+                        const indexUiState = uiState['aesseal us'] || {};
+                        return{
+                            q: indexUiState.query,
+                            type: indexUiState.refinementList && indexUiState.refinementList.type,
+                            lang: indexUiState.refinementList && indexUiState.refinementList.search_api_language
+                        }
+                    },
+                    routeToState(routeState) {
+                        return{
+                            ['aesseal us']: {
+                                query: routeState.q,
+                                refinementList: {
+                                    type: routeState.type,
+                                    search_api_language: routeState.lang
+                                }
+                            },
+                        };
+                    },
+                },
+            },
         })
         
         usaSearch.addWidgets([
