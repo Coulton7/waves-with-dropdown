02e8c828df45bbc827fcdfdad86e0b01201812ff
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-10-27 12:38:16 +0000

    New exprimental tab check js

---
+++ assets/js/search-tab.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var intResultsNum = document.querySelector('#intResultsNum');
+    var searchButton = document.querySelector('.ais-SearchBox-submit');
+    var searchInput = document.querySelector('.ais-SearchBox-input');
+
+    searchButton.addEventListener("click", function() {
+
+        if (intResultsNum.innerText.trim() === "0") {
+            document.querySelector('#globalResultsTab').classList.add('highlight-tab');
+        }
+
+    })
+
+    searchInput.addEventListener("keydown", function(e) {
+        if (e.key === 'Enter') {
+            if (intResultsNum.innerText.trim() === "0") {
+                document.querySelector('#globalResultsTab').classList.add('highlight-tab');
+            }
+
+        }
+    })
+
+});
