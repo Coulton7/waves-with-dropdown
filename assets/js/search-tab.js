93025270aef40c1c2c1b817ae6584c1e4c9a4c8f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-10-27 14:59:09 +0000

    Testing the js with a small timeout function

--- assets/js/search-tab.js
+++ assets/js/search-tab.js
@@ -5,22 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
     var searchInput = document.querySelector('.ais-SearchBox-input');
 
     searchButton.addEventListener("click", function() {
-
-        console.log(intResultsNum.innerHTML);
-
-        if (intResultsNum.innerText === "0") {
-            document.querySelector('#globalResultsTab').classList.add('highlight-tab');
-        }
-
-    })
-
-    searchInput.addEventListener("keydown", function(e) {
-        if (e.key === 'Enter') {
+        setTimeout(function() {
             console.log(intResultsNum.innerHTML);
+
             if (intResultsNum.innerText === "0") {
                 document.querySelector('#globalResultsTab').classList.add('highlight-tab');
             }
+        }, 1000);
+    });
 
+    searchInput.addEventListener("keydown", function(e) {
+        if (e.key === 'Enter') {
+            setTimeout(function() {
+                console.log(intResultsNum.innerHTML);
+                if (intResultsNum.innerText === "0") {
+                    document.querySelector('#globalResultsTab').classList.add('highlight-tab');
+                }
+
+            }, 1000);
         }
     })
 
