fc4af818af3e3cc63e5a9286d5b9c0196cbf36f8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-10-31 11:37:51 +0000

    Color adjustment of text and trialling new js adjustment

--- assets/js/search-tab.js
+++ assets/js/search-tab.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const intResultsNum = document.querySelector('#intResultsNum');
+    const globalResultsNum = document.querySelector('#globalResultsNum');
     var searchButton = document.querySelector('.ais-SearchBox-submit');
     var searchInput = document.querySelector('.ais-SearchBox-input');
 
     searchButton.addEventListener("click", function() {
         setTimeout(function() {
-            if (intResultsNum.innerText === "0") {
+            if (intResultsNum.innerText === "0" && globalResultsNum.innerText >= "1") {
                 document.querySelector('#globalResultsTab').classList.add('highlight-tab');
             } else {
                 document.querySelector('#globalResultsTab').classList.remove('highlight-tab');
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     searchInput.addEventListener("keydown", function(e) {
         if (e.key === 'Enter') {
             setTimeout(function() {
-                if (intResultsNum.innerText === "0") {
+                if (intResultsNum.innerText === "0" && globalResultsNum.innerText >= "1") {
                     document.querySelector('#globalResultsTab').classList.add('highlight-tab');
                 } else {
                     document.querySelector('#globalResultsTab').classList.remove('highlight-tab');
