a5e22bc01f540fe1d056ced82d791a15feb46d4a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-02-04 16:31:12 +0000

    Testing first print of the new search being built for pss

---
+++ assets/js/seal-selector.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const searchClient = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
+
+    window.dataLayer = window.dataLayer || [];
+    const { connectClearRefinements } = instantsearch.connectors;
+
+    const renderClearRefinements = (renderOptions, isFirstRender) => {
+        const {canRefine, refine, widgetParams} = renderOptions;
+        if(isFirstRender) {
+            const clearButton = document.createElement('button');
+            clearButton.classList.add("btn", "btn-primary", "mt-5");
+            clearButton.textContent = 'Reset';
+            clearButton.addEventListener('click', () => {
+                refine();
+            });
+            widgetParams.container.appendChild(clearButton);
+        }
+        widgetParams.container.querySelector('button').disabled = !canRefine;
+    }
+
+    const customClearRefinements = connectClearRefinements(renderClearRefinements);
+
+    window.dataLayer.push({
+        algoliaUserToken: 'user-1',
+    });
+
+    const sealSelector = instantsearch({
+        searchClient,
+        indexName: 'pss_seal_selector',
+        typoTolerance: 'strict',
+        paginationLimitedTo: 80,
+        searchFunction(helper) {
+                if (helper.state.query === '')
+                {
+                    return;
+                }
+                helper.search();
+            },
+            
+            insights: {
+                onEvent(event) {
+                    const { widgetType, eventType, payload, hits } = event;
+                    if (widgetType == 'ais.hits' && eventType === 'view') {
+                        dataLayer.push({ event: 'Hits Viewed' });
+                    }
+                }
+            },
+        });
+        
+    sealSelector.addWidgets([
+        customClearRefinements({
+                container: document.querySelector('#reset-options'),
+            }),
+
+        instantsearch.widgets.refinementList({
+            container: document.querySelector("#measurementType"),
+            attribute: "Measurement",
+        }),
+        instantsearch.widgets.refinementList({
+            container: document.querySelector("#shaftDiameter"),
+            attribute: "Shaft Diameter*",
+        }),
+        instantsearch.widgets.refinementList({
+            container: document.querySelector("#sternTube"),
+            attribute: "Stern Tube Diameter*",
+        }),
+        instantsearch.widgets.hits({
+            cotainer: document.querySelector("#seal-seclection"),
+        })
+
+    ])
+    
+});
