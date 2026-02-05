7697c74626eba72fb62e73530cfb769eda9195dd
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-02-05 11:01:59 +0000

    Changing to a connector instead of widget

--- assets/js/seal-selector.js
+++ assets/js/seal-selector.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.dataLayer = window.dataLayer || [];
     const { connectClearRefinements } = instantsearch.connectors;
-    const { createDropdown } = instantsearch.widgets;
+    const { createDropdown } = instantsearch.connectors;
 
     const renderClearRefinements = (renderOptions, isFirstRender) => {
         const {canRefine, refine, widgetParams} = renderOptions;
