cff73ed47ffeb53034301ce35f5326b83cd16030
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-03-05 10:44:45 +0000

    Trying new js to see if tooltip can be replaced with hover

--- assets/js/init-bs.js
+++ assets/js/init-bs.js
@@ -21,3 +21,8 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector('.navbar-toggler').classList.add("flag-space");
     }
 });
+
+const buttonExpandTooltip = document.getElementById('enquiry-icon');
+const expandTooltip = new bootstrap.Tooltip(buttonExpandTooltip, {
+    trigger: hover,
+});
