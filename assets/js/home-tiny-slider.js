66958a32471d3f3dac19ba50595fbe86f976396c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-03-10 16:08:50 +0000

    Reset

--- assets/js/home-tiny-slider.js
+++ assets/js/home-tiny-slider.js
@@ -246,9 +246,11 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    if(document.querySelectorAll('.test-slider').length > 0){
         var dataSlider = document.querySelector('.test-slider');
         opt = JSON.parse(dataSlider.getAttribute('data-tiny-slider'));
         opt.container = dataSlider;
 
         var test_slider = tns ({opt})
+    }
 });
