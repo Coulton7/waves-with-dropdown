992b5e49d367d8d866db68f6446be50f895ad74a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-03-31 09:13:53 +0100

    Trialing base js and content of the new timeline scroll

---
+++ assets/js/timeline-scroll.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var timeline = document.querySelector(".timeline");
+    var timelineImage = document.querySelector(".timeline-image");
+    var scrollPos = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    var topofImage = timeline.getBoundingClientRect().top;
+    var imageWidth = timelineImage.naturalWidth;
+
+    timeline.style.height=imageWidth
+
+    window.addEventListener('scroll', function(){
+        if (topofImage <= 0) {
+            timeline.style.display = "sticky";
+            timeline.style.transform = 'translateX(' + (-scrollPos) + 'px)';
+        } else {
+            timeline.style.display = "block";
+        }
+    });
+});
