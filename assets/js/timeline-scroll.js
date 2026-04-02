bc95cdc13e7321af6e449d6f30bae3d390da8635
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-04-02 16:30:29 +0100

    Moving one of the vars to other one

--- assets/js/timeline-scroll.js
+++ assets/js/timeline-scroll.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var timeline = document.querySelector(".timeline");
     var timelineContainer = document.querySelector(".timeline-container");
     var timelineImage = document.querySelector(".timeline-image");
-    var scrollPos = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
     var imageWidth = timelineImage.clientWidth;
 
     timeline.setAttribute("style", "height: " + imageWidth + "px");
 
     window.addEventListener('scroll', function(){
+        var scrollPos = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
         if (timeline.getBoundingClientRect().top < 0) {
             timelineContainer.style.position = "sticky";
             timelineContainer.style.top = "0";
