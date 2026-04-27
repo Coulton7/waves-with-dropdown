ca9efe508e2ae796eeb1392f029abbc25d12c1d1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-04-27 11:53:15 +0100

    Testing to see the value to check if equation works

--- assets/js/timeline-scroll.js
+++ assets/js/timeline-scroll.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 } else if(scrolled >= lastScrollTop) {
                     scrollValue += 1; 
                     lastScrollTop = scrolled;
+                    console.log(imageWidth /10);
                     if(scrollValue > imageWidth / 10) {
                         scrollValue = imageWidth / 10;
                     }
