f0835cade94449174af8013820c6595a50415be1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-04-29 14:28:45 +0100

    Trying with new listener to see the result

--- assets/js/timeline-scroll.js
+++ assets/js/timeline-scroll.js
@@ -1,64 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var timeline = document.querySelector(".timeline");
-    var timelineContainer = document.querySelector(".timeline-container");
-    var timelineImage = document.querySelector(".timeline-image");
-    var imageWidth = timelineImage.clientWidth;
-    var fifthOfImageWidth = imageWidth / 5;
-    var scrollValue = 0;
-    var lastScrollTop = 0;
+    if (document.querySelector('.timeline')) {
+        var timeline = document.querySelector(".timeline");
+        var timelineContainer = document.querySelector(".timeline-container");
+        var timelineImage = document.querySelector(".timeline-image");
+        var imageWidth = timelineImage.clientWidth;
+        var fifthOfImageWidth = imageWidth / 5;
+        var scrollValue = 0;
+        var lastScrollTop = 0;
 
-    timeline.setAttribute("style", "height: " + imageWidth + "px");
+        timeline.setAttribute("style", "height: " + imageWidth + "px");
 
-    window.addEventListener('scroll', function(e){
-        var timelineBounding = timeline.getBoundingClientRect();
-        var navBounding = document.getElementById("autocollapse").getBoundingClientRect();
-        const scrolled = window.scrollY;
-        
-        if (timelineBounding.top < navBounding.bottom) {
-            timelineContainer.style.position = "sticky";
-            timelineContainer.style.top = "5rem";
-            timelineImage.style.position = "absolute";
-        } else if (timelineBounding.top > navBounding.bottom
-        ) {
-            timelineContainer.style.position = "relative";
-            timelineContainer.style.top = "0";
-            timelineImage.style.position = "relative";
-        }
+        window.addEventListener('scroll', function(e){
+            var timelineBounding = timeline.getBoundingClientRect();
+            var navBounding = document.getElementById("autocollapse").getBoundingClientRect();
+            const scrolled = window.scrollY;
+            
+            if (timelineBounding.top < navBounding.bottom) {
+                timelineContainer.style.position = "sticky";
+                timelineContainer.style.top = "5rem";
+                timelineImage.style.position = "absolute";
+            } else if (timelineBounding.top > navBounding.bottom
+            ) {
+                timelineContainer.style.position = "relative";
+                timelineContainer.style.top = "0";
+                timelineImage.style.position = "relative";
+            }
 
-        if(timelineBounding.bottom < navBounding.bottom) {
-            timelineContainer.style.position = "relative";
-            timelineContainer.style.top = "0";
-            timelineImage.style.position = "relative";
-        } else if (timelineBounding.bottom < navBounding.bottom) {
-            timelineContainer.style.position = "sticky";
-            timelineContainer.style.top = "5rem";
-            timelineImage.style.position = "absolute";
-        }
+            if(timelineBounding.bottom < navBounding.bottom) {
+                timelineContainer.style.position = "relative";
+                timelineContainer.style.top = "0";
+                timelineImage.style.position = "relative";
+            } else if (timelineBounding.bottom < navBounding.bottom) {
+                timelineContainer.style.position = "sticky";
+                timelineContainer.style.top = "5rem";
+                timelineImage.style.position = "absolute";
+            }
 
-        if (timelineContainer.style.position === "sticky" ){
-
+            if (timelineContainer.style.position === "sticky" ){
                 var timelineTop = timelineBounding.top;
                 var totalScrollableHeight = timeline.scrollHeight; - timeline.clientHeight;
                 var scrollPercent = ((timelineTop / totalScrollableHeight) * 100);
-                console.log("Percentage: " + scrollPercent);
-            
-                if (scrolled <= lastScrollTop) {                    
-                    scrollValue -= 1;
-                    lastScrollTop = scrolled;
-                    if(scrollValue < 0) {
-                        scrollValue = 0;
-                    }
-                    console.log(scrollValue);
-                } else if(scrolled >= lastScrollTop) {
-                    scrollValue += 1; 
-                    lastScrollTop = scrolled;
-                    if(scrollValue > imageWidth / 10) {
-                        scrollValue = imageWidth / 10;
-                    }
-                }
                 timelineImage.style.transform = 'translateX(' + Math.min(0, Math.max(-80, scrollPercent)) + '%)';
             }
-        
-        });
             
-    });
+        });
+
+    }
+            
+});
