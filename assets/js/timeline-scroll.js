ce3a3688190e413dcb8e5769c7d32e1f8414adcc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2026-04-30 15:03:03 +0100

    Adding eager loading to the image

--- assets/js/timeline-scroll.js
+++ assets/js/timeline-scroll.js
@@ -8,42 +8,40 @@ document.addEventListener("DOMContentLoaded", function() {
         var scrollValue = 0;
         var lastScrollTop = 0;
 
-        if (!(timeline.style.height === "0px")) {
-            window.addEventListener('scroll', function(e){
-                var timelineBounding = timeline.getBoundingClientRect();
-                var navBounding = document.getElementById("autocollapse").getBoundingClientRect();
-                const scrolled = window.scrollY;
-                
-                if (timelineBounding.top < navBounding.bottom) {
-                    timelineContainer.style.position = "sticky";
-                    timelineContainer.style.top = "5rem";
-                    timelineImage.style.position = "absolute";
-                } else if (timelineBounding.top > navBounding.bottom
-                ) {
-                    timelineContainer.style.position = "relative";
-                    timelineContainer.style.top = "0";
-                    timelineImage.style.position = "relative";
-                }
+        window.addEventListener('scroll', function(e){
+            var timelineBounding = timeline.getBoundingClientRect();
+            var navBounding = document.getElementById("autocollapse").getBoundingClientRect();
+            const scrolled = window.scrollY;
+            
+            if (timelineBounding.top < navBounding.bottom) {
+                timelineContainer.style.position = "sticky";
+                timelineContainer.style.top = "5rem";
+                timelineImage.style.position = "absolute";
+            } else if (timelineBounding.top > navBounding.bottom
+            ) {
+                timelineContainer.style.position = "relative";
+                timelineContainer.style.top = "0";
+                timelineImage.style.position = "relative";
+            }
 
-                if(timelineBounding.bottom < navBounding.bottom) {
-                    timelineContainer.style.position = "relative";
-                    timelineContainer.style.top = "0";
-                    timelineImage.style.position = "relative";
-                } else if (timelineBounding.bottom < navBounding.bottom) {
-                    timelineContainer.style.position = "sticky";
-                    timelineContainer.style.top = "5rem";
-                    timelineImage.style.position = "absolute";
-                }
+            if(timelineBounding.bottom < navBounding.bottom) {
+                timelineContainer.style.position = "relative";
+                timelineContainer.style.top = "0";
+                timelineImage.style.position = "relative";
+            } else if (timelineBounding.bottom < navBounding.bottom) {
+                timelineContainer.style.position = "sticky";
+                timelineContainer.style.top = "5rem";
+                timelineImage.style.position = "absolute";
+            }
 
-                if (timelineContainer.style.position === "sticky" ){
-                    var timelineTop = timelineBounding.top;
-                    var totalScrollableHeight = timeline.scrollHeight; - timeline.clientHeight;
-                    var scrollPercent = ((timelineTop / totalScrollableHeight) * 100);
-                    timelineImage.style.transform = 'translateX(' + Math.min(0, Math.max(-80, scrollPercent)) + '%)';
-                }
-                
-            });
+            if (timelineContainer.style.position === "sticky" ){
+                var timelineTop = timelineBounding.top;
+                var totalScrollableHeight = timeline.scrollHeight; - timeline.clientHeight;
+                var scrollPercent = ((timelineTop / totalScrollableHeight) * 100);
+                timelineImage.style.transform = 'translateX(' + Math.min(0, Math.max(-80, scrollPercent)) + '%)';
+            }
+            
+        });
 
-        }
     }   
 });
